74ca4525e047ffaa9753f7e6452cc76aadd70e65
Julien <julient.bourgeois@gmail.com>
2024-07-04 10:06:34 +0200

    add logger

--- src/main/java/com/julien/paymybuddy/controller/UserConnectionController.java
+++ src/main/java/com/julien/paymybuddy/controller/UserConnectionController.java
@@ -6,6 +6,8 @@ import com.julien.paymybuddy.exception.ConnectionAlreadyExistException;
 import com.julien.paymybuddy.exception.UserNotFoundException;
 import com.julien.paymybuddy.service.UserConnectionService;
 import jakarta.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,6 +24,8 @@ public class UserConnectionController {
         this.userConnectionService = userConnectionService;
     }
 
+    private static final Logger logger = LoggerFactory.getLogger(UserConnectionController.class);
+
     @GetMapping("/get-connection")
     public ResponseEntity<List<UserConnectionDTO>> getCurrentUserConnections(HttpSession session) {
         UserEntity currentUser = (UserEntity) session.getAttribute("user");
@@ -34,13 +38,17 @@ public class UserConnectionController {
             List<UserConnectionDTO> connectionsForUser = userConnectionService.getConnectionsForUser(currentUser.getUserId());
 
             if (null != connectionsForUser) {
+                logger.debug("Connections for user {} found", currentUser.getUserId());
                 return ResponseEntity.ok(connectionsForUser);
             } else {
+                logger.debug("Connections for user {} found but user don't have connections", currentUser.getUserId());
                 return ResponseEntity.noContent().build();
             }
         } catch (UserNotFoundException e) {
+            logger.error("User not found", e);
             return ResponseEntity.notFound().build();
         } catch (Exception e) {
+            logger.error("Unexpected error", e);
             return ResponseEntity.internalServerError().build();
         }
     }
@@ -50,13 +58,16 @@ public class UserConnectionController {
         UserEntity currentUser = (UserEntity) session.getAttribute("user");
 
         if (currentUser == null) {
+            logger.error("User not found on session");
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
 
         try {
             List<String> connectionsNamesForUser = userConnectionService.getConnectionsNamesForUser(currentUser.getUserId());
+            logger.debug("Connections usernames for user {} found", currentUser.getUserId());
             return ResponseEntity.ok(connectionsNamesForUser);
         } catch (Exception e) {
+            logger.error("Unexpected error", e);
             return ResponseEntity.internalServerError().build();
         }
     }
@@ -66,17 +77,22 @@ public class UserConnectionController {
         UserEntity currentUser = (UserEntity) session.getAttribute("user");
 
         if (currentUser == null) {
+            logger.error("User not found on session");
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
 
         try {
             UserConnectionDTO userConnectionDTO = userConnectionService.addConnection(currentUser.getUserId(), userToConnectMail);
+            logger.debug("Connection {} added", currentUser.getUserId());
             return ResponseEntity.ok(userConnectionDTO);
         } catch (ConnectionAlreadyExistException e) {
+            logger.error("Connection {} already exists", currentUser.getUserId());
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
         }catch (UserNotFoundException e) {
+            logger.error("User not found", e);
             return ResponseEntity.notFound().build();
         } catch (Exception e) {
+            logger.error("Unexpected error", e);
             return ResponseEntity.internalServerError().build();
         }
     }
